38f1f0cebbc3a4af984682a165f8df0e11e7740f
kiran879 <rajascricketer@gmail.com>
2025-01-03 08:13:27 +0000

    Initial commit: Includes necessary kernel and driver headers.

---
+++ headers.h
@@ -0,0 +1,9 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/fs.h>
+#include<linux/moduleparam.h>
+#include<linux/slab.h>
+#include<linux/cdev.h>
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("My character device driver-1");
+MODULE_AUTHOR("rk");
