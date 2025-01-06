2506969ad6cd25910ded3618e154a5d896dc748b
kiran879 <rajascricketer@gmail.com>
2025-01-06 04:57:50 +0000

    Initial commit: Implements trim operation on the device

---
+++ trimMyCDD.c
@@ -0,0 +1,10 @@
+#include"headers.h"
+#include"declarations.h"
+int trimMyCDD(Device *ldev)
+{
+	printk(KERN_INFO "FILE:%s -> %s:Begin\n",__FILE__,__func__);
+
+
+	printk(KERN_INFO "FILE:%s -> %s:End\n",__FILE__,__func__);
+	return 0;
+}
