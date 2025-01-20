f13aa12465b5bdead1e1bdae157c1da9ee48ac47
kiran879 <rajascricketer@gmail.com>
2025-01-20 15:39:52 +0000

    Updated: Added new lines of code(Implementation)

--- writeMyCDD.c
+++ writeMyCDD.c
@@ -2,7 +2,7 @@
 #include"declarations.h"
 ssize_t writeMyCDD(struct file *pfi, const char __user *ubuff, size_t size, loff_t *offset)
 {
-	int lsize,rem_size,wret,nob_tocopy;
+	int i,j,no_quantums,lsize,rem_size,wret,nob_tocopy;
 	Device *ldev;
 	Item *l_item;
 	printk(KERN_INFO "FILE:%s -> %s:Begin\n",__FILE__,__func__);
@@ -19,14 +19,14 @@ ssize_t writeMyCDD(struct file *pfi, const char __user *ubuff, size_t size, loff
 		no_quantums++;
 	//write operation
 	rem_size=lsize;
-	for(i=j=0,j<no_quantums;j++)
+	for(i=j=0;j<no_quantums;j++)
 	{
 		nob_tocopy=ldev->regSize;//no. of bytes to copy
 		if(rem_size<ldev->regSize)
 		{	
-			no_tocopy=rem_size;
+			nob_tocopy=rem_size;
 		}
-		wret=copy_from_user(ldev->item->data[i],__user ubuff,nob_tocopy);
+		wret=copy_from_user(l_item->data[i],__user ubuff,nob_tocopy);
 		if(wret==-1)
 		{
 			trimMyCDD(ldev);
@@ -34,16 +34,16 @@ ssize_t writeMyCDD(struct file *pfi, const char __user *ubuff, size_t size, loff
 			goto out;
 		}
 		rem_size = rem_size - (ldev->regSize-wret);//remaining size/bytes to copy;
-		if(i==ldev->regSize-1)//if all the quantums in a item are filled
+		if(i==ldev->noofReg-1)//if all the quantums in a item are filled
 		{
 			i=0;
-			item=item->next;
+			l_item=l_item->next;
 		}
 		else
 			i++;
-
-
 	}
+	printk(KERN_INFO "Data at 1st quantum:%s\n",ldev->item->data[0]);
+	printk(KERN_INFO "Data at 2nd quantum:%s\n",ldev->item->next->data[2]);
 
 out:
 	printk(KERN_INFO "FILE:%s -> %s:End\n",__FILE__,__func__);
