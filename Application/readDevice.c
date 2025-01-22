222243cd2c7b6c690a4cf5d9e22b96da7e1876ec
kiran879 <rajascricketer@gmail.com>
2025-01-22 21:00:04 +0000

    Updated: Added new line of code(memset)

--- Application/readDevice.c
+++ Application/readDevice.c
@@ -2,11 +2,20 @@
 #include"f_declarations.h"
 void* readDevice(void* arg)
 {
-	char buff[10];
+	int ret;
+	char buff[100];
 #ifdef DEBUG
 	printf("F_Operations: FILE:%s-> %s:Begin\n",__FILE__,__func__);
 #endif
-	read(fd,buff,5);
+	memset(buff,'\0',100);
+	ret=read(fd,buff,100);
+	if(ret==-1)
+	{
+		perror("read");
+		(*f_fptr[1])((void*)"failure");
+	}
+	printf("no.of bytes read:%d\n",ret);
+	printf("Data read fromt he device: %s\n",buff);
 #ifdef DEBUG
 	printf("F_Operations: FILE:%s-> %s:End\n",__FILE__,__func__);
 #endif
