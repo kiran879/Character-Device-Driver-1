eca68d4032af4a339b6f234d05bbec32db36778a
kiran879 <rajascricketer@gmail.com>
2025-01-22 20:54:28 +0000

    Updated: corrected functionality(ubuff offset)

--- writeMyCDD.c
+++ writeMyCDD.c
@@ -2,7 +2,7 @@
 #include"declarations.h"
 ssize_t writeMyCDD(struct file *pfi, const char __user *ubuff, size_t size, loff_t *offset)
 {
-	int i,j,no_quantums,lsize,rem_size,wret,nob_tocopy;
+	int i,j,no_quantums,lsize,rem_size,wret,nob_tocopy,nob_wrote;
 	Device *ldev;
 	Item *l_item;
 	printk(KERN_INFO "FILE:%s -> %s:Begin\n",__FILE__,__func__);
@@ -13,12 +13,18 @@ ssize_t writeMyCDD(struct file *pfi, const char __user *ubuff, size_t size, loff
 		lsize=ldev->devSize;
 
 	l_item=ldev->item=createScull(ldev,lsize);//creating scull
+	if(l_item==0)
+	{
+		printk(KERN_ERR "Error: SCULL creation failed!\n");
+		goto out;
+	}
 	//calculating no. of quantums required
 	no_quantums=lsize/ldev->regSize;
 	if(no_quantums%ldev->regSize != 0)
 		no_quantums++;
 	//write operation
 	rem_size=lsize;
+	nob_wrote=0;
 	for(i=j=0;j<no_quantums;j++)
 	{
 		nob_tocopy=ldev->regSize;//no. of bytes to copy
@@ -26,14 +32,16 @@ ssize_t writeMyCDD(struct file *pfi, const char __user *ubuff, size_t size, loff
 		{	
 			nob_tocopy=rem_size;
 		}
-		wret=copy_from_user(l_item->data[i],__user ubuff,nob_tocopy);
+		wret=copy_from_user(l_item->data[i],ubuff+nob_wrote,nob_tocopy);
 		if(wret==-1)
 		{
 			trimMyCDD(ldev);
 			printk(KERN_ERR "Error: copy_from_user failed!\n");
 			goto out;
 		}
-		rem_size = rem_size - (ldev->regSize-wret);//remaining size/bytes to copy;
+
+		rem_size = rem_size - (nob_tocopy-wret);//remaining size/bytes to copy;
+		nob_wrote=nob_wrote+nob_tocopy-wret;
 		if(i==ldev->noofReg-1)//if all the quantums in a item are filled
 		{
 			i=0;
@@ -42,10 +50,13 @@ ssize_t writeMyCDD(struct file *pfi, const char __user *ubuff, size_t size, loff
 		else
 			i++;
 	}
-	printk(KERN_INFO "Data at 1st quantum:%s\n",ldev->item->data[0]);
-	printk(KERN_INFO "Data at 2nd quantum:%s\n",ldev->item->next->data[2]);
-
+	//printk(KERN_INFO "Data at 1st quantum:%s\n",ldev->item->data[0]);
+	//printk(KERN_INFO "Data at 2nd quantum:%s\n",ldev->item->next->data[2]);
+	
+	ldev->dataSize=nob_wrote;
+	printk(KERN_INFO "FILE:%s -> %s:End\n",__FILE__,__func__);
+	return nob_wrote;
 out:
 	printk(KERN_INFO "FILE:%s -> %s:End\n",__FILE__,__func__);
-	return 0;
+	return -1;
 }
